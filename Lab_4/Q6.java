1fa8d41c50d40bd709e64572f24f0083496aad84
Kumar-Snehal <kssnehal605@gmail.com>
2026-02-02 17:06:58 +0530

    started part 6

---
+++ Lab_4/Q6.java
@@ -0,0 +1,44 @@
+package Lab_4;
+
+class User {
+
+    private String userId, password, loginStatus;
+    private dateformat registerDate;
+
+    public Boolean verifyLogin() {
+        Boolean ans = false;
+        return ans;
+    }
+}
+
+class Administrator extends User {
+
+    private String adminName, email;
+
+    public Boolean updateCatalog() {
+        Boolean ans = false;
+        return ans;
+    }
+}
+
+class Customer extends User {
+
+    private String customerName, address, email, creditCardInfo, shippingInfo;
+    private float accountBalance;
+
+    public void register() {
+
+    }
+
+    public void login() {
+
+    }
+
+    public void updateProfile() {
+
+    }
+}
+
+public class Q6 {
+
+}
---
+++ Lab_4/dateformat.java
@@ -0,0 +1,24 @@
+package Lab_4;
+
+public class dateformat {
+
+    public int day;
+    public int month;
+    public int year;
+
+    dateformat() {
+        day = 1;
+        month = 1;
+        year = 1;
+    }
+
+    public dateformat(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public void display() {
+        System.out.println("" + day + "/" + month + "/" + year);
+    }
+}
