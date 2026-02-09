006e27d09c52134f541de34e6a0284fb524e7ed5
Kumar-Snehal <kssnehal605@gmail.com>
2026-02-09 17:00:25 +0530

    finished q7

--- Lab_5/Q6.java
+++ Lab_5/Q6.java
@@ -3,8 +3,32 @@ package Lab_5;
 import java.util.*;
 
 public class Q6 {
+
+    static void PrintList(List l) {
+        Iterator it = l.iterator();
+        while (it.hasNext())
+            System.out.print(" " + it.next());
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         List<Integer> postIds = new ArrayList<>(
                 Arrays.asList(1, 2, 3, 4, 5, 0, 2, 3, 1, 3, 5, 78, 6, 3, 2, 43, 65, 34, 2, 4, 5, 6, 23));
+
+        System.out.println("Liked Post IDs:");
+        PrintList(postIds);
+
+        LinkedHashSet<Integer> s = new LinkedHashSet<>();
+        for (Integer id : postIds) {
+            s.add(id);
+        }
+
+        List<Integer> uniquePostIds = new ArrayList<>();
+        for (Integer it : s) {
+            uniquePostIds.add(it);
+        }
+
+        System.out.println("Unique Liked Post IDs:");
+        PrintList(uniquePostIds);
     }
 }
--- Lab_5/Q7.java
+++ Lab_5/Q7.java
@@ -2,6 +2,68 @@ package Lab_5;
 
 import java.util.*;
 
-public class Q7 {
+class Stack<T> {
+    private class Node<U> {
+        U data;
+        Node<U> next;
 
+        public Node(U data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    Node<T> top;
+    private int size;
+
+    public Stack() {
+        top = null;
+        size = 0;
+    }
+
+    public void push(T item) {
+        Node<T> newTop = new Node<T>(item);
+        newTop.next = top;
+        top = newTop;
+        size++;
+    }
+
+    public T pop() {
+        if (size == 0 || top == null)
+            return null;
+        T retVal = top.data;
+        top = top.next;
+        size--;
+        return retVal;
+    }
+
+    public T peek() {
+        return top.data;
+    }
+
+    public Boolean isEmpty() {
+        return (size == 0);
+    }
+}
+
+public class Q7 {
+    static <T> void sampleTest(T ar[]) {
+        int n = ar.length;
+        Stack<T> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            System.out.println("+Pushed " + ar[i]);
+            st.push(ar[i]);
+        }
+        for (int i = 0; !st.isEmpty() && i < n / 3; i++) {
+            System.out.println("-Popped " + st.pop());
+        }
+        System.out.println("Stack top: " + st.peek());
+    }
+
+    public static void main(String[] args) {
+        System.out.println("\nString Stack:");
+        sampleTest(new String[] { "Aa", "Bb", "Cc", "Dd", "Ee", "Ff", "Gg", "Hh" });
+        System.out.println("\nInteger Stack:");
+        sampleTest(new Integer[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 });
+    }
 }
