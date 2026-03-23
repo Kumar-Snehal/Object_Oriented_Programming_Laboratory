af59962183fea75ff9c479a88bc8f5189fa87f9c
Kumar-Snehal <kssnehal605@gmail.com>
2026-03-23 16:38:36 +0530

    added base for section 2: Flight_Mgnt_System

---
+++ Lab_8/Flight_Mgnt_System/FlightControl.java
@@ -0,0 +1,11 @@
+package Flight_Mgnt_System;
+
+public class FlightControl {
+    public FlightControl() {
+
+    }
+
+    public void request() {
+
+    }
+}
---
+++ Lab_8/Flight_Mgnt_System/Main.java
@@ -0,0 +1,11 @@
+package Flight_Mgnt_System;
+
+public class Main {
+    public static void main(String[] args) {
+        FlightControl control = new FlightControl();
+        control.request();
+        control.request();
+        control.request();
+        control.request();
+    }
+}
